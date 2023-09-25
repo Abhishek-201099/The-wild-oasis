98e7ceb09f05416811221ae3b42c298271e806ed
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2023-09-25 18:13:24 +0530

    feat: Implement form functionality with React Hook Form - Installed the 'react-hook-form' npm package for form functionality. - Utilized the useForm() hook to manage form state, validation, and submission. - Created the 'createCabin' function in services to add new cabin data to Supabase. - Integrated the useMutation() hook for 'createCabin' and added toast notifications for success and failure states. - Implemented validation using the 'register()' function for required fields and values.

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,6 +9,20 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Failed to create new cabin.");
+  }
+
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
